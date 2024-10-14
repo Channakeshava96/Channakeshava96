2941a06a3da57e989b70d0fb7c63c6bfda107904
Channakeshava96 <channa964348@gmail.com>
2024-10-14 17:38:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger');
+const navBar = document.querySelector('#navbar');
+
+burger.addEventListener('click', () => {
+    navBar.classList.toggle('active');
+});
